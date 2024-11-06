e00fc2c0ad4acc2244d76f0b448b5f95647ca221
Viorel Budeci <viorel.budeci@cegeka.com>
2024-11-06 18:01:54 +0200

    comment more code

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.103.1"
-    }
-  }
+  # required_providers {
+  #   azurerm = {
+  #     source  = "hashicorp/azurerm"
+  #     version = "3.103.1"
+  #   }
+  # }
   backend "azurerm" {
     resource_group_name  = "rg-viqub-shared-we-01"
     storage_account_name = "stviqubsharedwe02"
@@ -13,8 +13,8 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  subscription_id = "4547b264-f1ab-4b5a-8e37-fd20926e38eb"
-  features {}
-}
+# provider "azurerm" {
+#   subscription_id = "4547b264-f1ab-4b5a-8e37-fd20926e38eb"
+#   features {}
+# }
 
