ea697dfa7129af9d1648c88225d78655a306979f
Viorel Budeci <viorel.budeci@cegeka.com>
2024-11-06 16:40:27 +0200

    add tenantId for provider

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -18,6 +18,7 @@ terraform {
 provider "azurerm" {
   storage_use_azuread = true
   subscription_id     = "4547b264-f1ab-4b5a-8e37-fd20926e38eb"
+  tenant_id           = "42151053-0193-47aa-9e81-effd81f772cc"
   features {}
 }
 
