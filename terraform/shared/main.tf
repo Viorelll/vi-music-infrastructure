360bf4d6dd1f8ddd612b8fc98a005b9d253b5f71
Viorel Budeci <viorel.budeci@cegeka.com>
2024-11-06 15:23:50 +0200

    change backend

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -11,6 +11,7 @@ terraform {
     container_name       = "tfstateshared"
     key                  = "terraform.tfstate"
     subscription_id      = "4547b264-f1ab-4b5a-8e37-fd20926e38eb"
+    tenant_id            = "42151053-0193-47aa-9e81-effd81f772cc"
   }
 }
 
