b636139dd6987ad47cd8b823750a5426e6103360
Viorel Budeci <viorel.budeci@cegeka.com>
2024-11-06 15:37:55 +0200

    use azure_ad for share storage account

--- terraform/shared/main.tf
+++ terraform/shared/main.tf
@@ -16,7 +16,8 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "4547b264-f1ab-4b5a-8e37-fd20926e38eb"
+  storage_use_azuread = true
+  subscription_id     = "4547b264-f1ab-4b5a-8e37-fd20926e38eb"
   features {}
 }
 
