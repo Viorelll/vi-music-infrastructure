b026bd7d28de6a95c8da997ca752e2a4d7fd095e
Viorel Budeci <viorel.budeci@cegeka.com>
2024-11-14 18:55:30 +0200

    set 'westeurope' for static web app

--- terraform/modules/environment/static-web-app.tf
+++ terraform/modules/environment/static-web-app.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_web_app" "vimusic_ui" {
   name                = "stapp-${var.application_name}-${var.environment_name}-${var.region_identifier}"
   resource_group_name = data.azurerm_resource_group.this.name
-  location            = data.azurerm_resource_group.this.location
+  location            = "westeurope" //data.azurerm_resource_group.this.location
 }
