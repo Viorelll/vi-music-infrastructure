6ffcbcb3123ae8741262943dfb41e7bde45cd1d3
Viorel Budeci <viorel.budeci@cegeka.com>
2024-11-14 15:30:19 +0200

    Revert "comment sql db"
    This reverts commit 00ba1f5a53a13169e728ac7505e31458384d9bc0.

---
+++ terraform/modules/environment/keyvault.tf
@@ -0,0 +1,26 @@
+resource "azurerm_key_vault" "this" {
+  name                      = "kv${var.application_name}${var.environment_name}${var.region_identifier}"
+  location                  = data.azurerm_resource_group.this.location
+  resource_group_name       = data.azurerm_resource_group.this.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true
+}
+
+resource "random_password" "sqldb_admin_password" {
+  length           = 16
+  special          = true
+  override_special = "!#*()-_+?"
+}
+
+resource "azurerm_key_vault_secret" "sqldb_admin_password" {
+  name         = "sqldb-admin-password"
+  key_vault_id = azurerm_key_vault.this.id
+  value        = random_password.sqldb_admin_password.result
+}
+
+resource "azurerm_key_vault_secret" "sqldb_connection_string_viqupapp" {
+  name         = "sqldb-connectionstring"
+  key_vault_id = azurerm_key_vault.this.id
+  value        = "Server=${azurerm_postgresql_flexible_server.server.name};Database=${azurerm_postgresql_flexible_server_database.db.name};Port=5432;User Id=${var.sqldb_admin_username};Password=${azurerm_postgresql_flexible_server.server.administrator_password};Ssl Mode=Require;"
+}
