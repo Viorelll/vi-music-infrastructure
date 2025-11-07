ab5854926ffa7fe37e6b19e8f3c30137e6d208da
Viorel Budeci <viorel.budeci@cegeka.com>
2025-11-07 05:23:47 +0200

    set domain name label for public ip

--- terraform/shared/public_ip.tf
+++ terraform/shared/public_ip.tf
@@ -1,5 +1,6 @@
 resource "azurerm_public_ip" "application_gateway" {
   name                = "pip-${var.application_name}-${var.shared_identifier}-${var.region_identifier}-01"
+  domain_name_label   = var.application_name
   resource_group_name = data.azurerm_resource_group.shared_resource.name
   location            = data.azurerm_resource_group.shared_resource.location
   allocation_method   = "Static"
