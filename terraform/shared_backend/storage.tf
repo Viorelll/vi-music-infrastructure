6d53f5f2c6985c234ffc6d9ab125678ce9a4b8e3
Viorel Budeci <viorel.budeci@cegeka.com>
2025-11-13 05:21:21 +0200

    fix shared backend

--- terraform/shared_backend/storage.tf
+++ terraform/shared_backend/storage.tf
@@ -39,6 +39,6 @@ resource "azurerm_storage_container" "tfstateshared" {
 
 resource "azurerm_storage_container" "tfstate_dev" {
   name                  = "tfstate${var.application_name}${var.dev_identifier}"
-  storage_account_id    = data.azurerm_storage_account.shared_storage.id
+  storage_account_id    = azurerm_storage_account.sharedstorage.id
   container_access_type = "private"
 }
