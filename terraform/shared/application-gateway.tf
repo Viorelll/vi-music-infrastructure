461412f27269f145307deae26bce36bee3f20b81
Viorel Budeci <viorel.budeci@cegeka.com>
2025-11-13 21:10:38 +0200

    Update infra

--- terraform/shared/application-gateway.tf
+++ terraform/shared/application-gateway.tf
@@ -1,5 +1,25 @@
 # USE POST INFRASTRUCTURE RUN BECAUSE IS USED 'SHARED GATEWAY' AND FIRST MUST BE CREATE ENVIRONMENT(S) AND AFTER LINK WITH GATEWAY
 
+
+# IMPORT EXISTED RESOURCES
+data "azurerm_container_app_environment" "dev" {
+  provider            = azurerm.dev_subscription
+  resource_group_name = local.dev_resource_group_name
+  name                = local.dev_container_app_environment_name
+}
+
+data "azurerm_container_app" "api_dev" {
+  provider            = azurerm.dev_subscription
+  resource_group_name = local.dev_resource_group_name
+  name                = local.dev_container_app_name
+}
+
+data "azurerm_key_vault" "dev_certificates" {
+  provider            = azurerm.dev_subscription
+  name                = "kv${var.application_name}${var.dev_identifier}${var.region_identifier}${var.resource_number}"
+  resource_group_name = local.dev_resource_group_name
+}
+
 locals {
   frontend_ip_configuration_name = "default"
   frontend_port_name             = "https"
--- terraform/shared/datasources.tf
+++ terraform/shared/datasources.tf
@@ -10,23 +10,23 @@ data "azurerm_storage_account" "shared_storage" {
 
 # USE POST INFRASTRUCTURE RUN BECAUSE IS USED 'SHARED GATEWAY' AND FIRST MUST BE CREATE ENVIRONMENT(S) AND AFTER LINK WITH GATEWAY
 
-data "azurerm_container_app_environment" "dev" {
-  provider            = azurerm.dev_subscription
-  resource_group_name = local.dev_resource_group_name
-  name                = local.dev_container_app_environment_name
-}
+# data "azurerm_container_app_environment" "dev" {
+#   provider            = azurerm.dev_subscription
+#   resource_group_name = local.dev_resource_group_name
+#   name                = local.dev_container_app_environment_name
+# }
 
-data "azurerm_container_app" "api_dev" {
-  provider            = azurerm.dev_subscription
-  resource_group_name = local.dev_resource_group_name
-  name                = local.dev_container_app_name
-}
+# data "azurerm_container_app" "api_dev" {
+#   provider            = azurerm.dev_subscription
+#   resource_group_name = local.dev_resource_group_name
+#   name                = local.dev_container_app_name
+# }
 
-data "azurerm_key_vault" "dev_certificates" {
-  provider            = azurerm.dev_subscription
-  name                = "kv${var.application_name}${var.dev_identifier}${var.region_identifier}${var.resource_number}"
-  resource_group_name = local.dev_resource_group_name
-}
+# data "azurerm_key_vault" "dev_certificates" {
+#   provider            = azurerm.dev_subscription
+#   name                = "kv${var.application_name}${var.dev_identifier}${var.region_identifier}${var.resource_number}"
+#   resource_group_name = local.dev_resource_group_name
+# }
 
 data "azurerm_key_vault_secret" "dev_backend_api_certificate" {
   provider     = azurerm.dev_subscription
